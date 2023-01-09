cc4557a88664f51a0995c94532be3e23336f240d
anwjrrp33 <anwjrrp33@gmail.com>
2023-01-09 23:05:53 +0900

    feat: Security basic configuration

---
+++ src/main/java/com/bookstore/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.bookstore.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+            .httpBasic().disable()
+            .csrf().disable()
+            .cors().and()
+            .headers().frameOptions().disable().and()
+            .authorizeHttpRequests()
+            .antMatchers("/**").permitAll().and()
+            .build();
+    }
+}
