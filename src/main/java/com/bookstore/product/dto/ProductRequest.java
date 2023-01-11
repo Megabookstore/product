94d8d0b5babf1e7c2d6d8890601400872452b87e
anwjrrp33 <anwjrrp33@gmail.com>
2023-01-11 22:15:56 +0900

    feat: 제품 DTO 구현

--- src/main/java/com/bookstore/product/dto/ProductRequest.java
+++ src/main/java/com/bookstore/product/dto/ProductRequest.java
@@ -1,5 +1,40 @@
 package com.bookstore.product.dto;
 
+import com.bookstore.product.domain.Description;
+import com.bookstore.product.domain.Name;
+import com.bookstore.product.domain.Price;
+import com.bookstore.product.domain.Product;
+import com.bookstore.product.domain.ProductStatus;
+import com.bookstore.product.domain.Stock;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
 public class ProductRequest {
 
+    private String description;
+
+    private String name;
+
+    private Long stock;
+
+    private String productStatus;
+
+    private String thumbnail;
+
+    private Long price;
+
+    public Product toProduct() {
+        return Product.builder()
+            .description(new Description(description))
+            .name(new Name(name))
+            .stock(new Stock(stock))
+            .productStatus(ProductStatus.valueOf(productStatus))
+            .thumbnail(thumbnail)
+            .price(new Price(price))
+            .build();
+    }
 }
--- src/main/java/com/bookstore/product/dto/ProductResponse.java
+++ src/main/java/com/bookstore/product/dto/ProductResponse.java
@@ -1,5 +1,36 @@
 package com.bookstore.product.dto;
 
+import com.bookstore.product.domain.Product;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
 public class ProductResponse {
 
+    private Long id;
+
+    private String description;
+
+    private String name;
+
+    private Long stock;
+
+    private String productStatus;
+
+    private String thumbnail;
+
+    private Long price;
+
+    public ProductResponse(Product product) {
+        this.id = product.getId();
+        this.description = product.getDescription().getDescription();
+        this.name = product.getName().getName();
+        this.stock = product.getStock().getStock();
+        this.productStatus = product.getProductStatus().getValue();
+        this.thumbnail = product.getThumbnail();
+        this.price = product.getPrice().getPrice().longValue();
+    }
 }
