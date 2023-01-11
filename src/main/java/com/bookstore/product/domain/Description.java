217ec3ae875bb5de203a34ff1c28acc54e8e6287
anwjrrp33 <anwjrrp33@gmail.com>
2023-01-11 20:46:01 +0900

    test: 설명 테스트 코드 작성

---
+++ src/main/java/com/bookstore/product/domain/Description.java
@@ -0,0 +1,32 @@
+package com.bookstore.product.domain;
+
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@NoArgsConstructor
+@Getter
+public class Description {
+
+    private static final String DESCRIPTION_NOT_EMPTY = "설명은 빈 값을 허용하지 않습니다.";
+    @Column(length = 1000, nullable = false)
+    private String description;
+
+    public Description(String description) {
+        validate(description);
+        this.description = description;
+    }
+
+    private void validate(String description) {
+        validateDescriptionNotEmpty(description);
+    }
+
+    private void validateDescriptionNotEmpty(String description) {
+        if (Objects.isNull(description) || description.isEmpty()) {
+            throw new IllegalArgumentException(DESCRIPTION_NOT_EMPTY);
+        }
+    }
+}
---
+++ src/test/java/com/bookstore/product/domain/DescriptionTest.java
@@ -0,0 +1,18 @@
+package com.bookstore.product.domain;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+
+class DescriptionTest {
+
+    @DisplayName("설명이 빈 값이면 에러가 발생한다.")
+    @ParameterizedTest
+    @NullAndEmptySource
+    void validateNameNotEmpty(String description) {
+        assertThatThrownBy(() -> new Description(description))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+}
