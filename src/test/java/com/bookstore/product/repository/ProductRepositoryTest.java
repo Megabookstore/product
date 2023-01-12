ab01991943bcc367e1bb6828d9e59926eb9bc177
2HyeonHo <dpqlvkvk@naver.com>
2023-01-12 21:45:49 +0900

    refactor: product repository 테스트코드 리팩토링
    공통코드 분리, 수정된 객체에 대한 확실한 검증

--- src/test/java/com/bookstore/product/repository/ProductRepositoryTest.java
+++ src/test/java/com/bookstore/product/repository/ProductRepositoryTest.java
@@ -2,6 +2,7 @@ package com.bookstore.product.repository;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 import com.bookstore.product.domain.Description;
 import com.bookstore.product.domain.Name;
@@ -12,6 +13,8 @@ import com.bookstore.product.domain.Stock;
 import com.bookstore.product.dto.ProductRequest;
 import com.bookstore.product.exception.ProductNotFoundException;
 import javax.persistence.EntityManager;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +26,18 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 @AutoConfigureTestDatabase(replace = Replace.NONE)
 class ProductRepositoryTest {
 
+    private ProductRequest productRequest;
+    private Product product;
+
+    @BeforeEach
+    void setup() {
+        productRequest = new ProductRequest("더 나은 코드를 작성하는 간단하고 실질적인 테크닉",
+            "읽기 좋은 코드가 좋은코드다", 1_000L, ProductStatus.SELL.name(),
+            "https://image.aladin.co.kr/product/1610/82/cover500/897914914x_1.jpg", 18_000L);
+        product = productRepository.save(productRequest.toProduct());
+        em.clear();
+    }
+
     @Autowired
     private ProductRepository productRepository;
 
@@ -32,31 +47,17 @@ class ProductRepositoryTest {
     @Test
     @DisplayName("상품을 데이터베이스에 등록한다.")
     public void register() {
-        //given
-        ProductRequest productRequest = new ProductRequest("더 나은 코드를 작성하는 간단하고 실질적인 테크닉",
-            "읽기 좋은 코드가 좋은코드다", 1_000L, ProductStatus.SELL.name(),
-            "https://image.aladin.co.kr/product/1610/82/cover500/897914914x_1.jpg", 18_000L);
-        Product saveProduct = productRepository.save(productRequest.toProduct());
-        em.clear();
-
         //when
-        Product findProduct = productRepository.findById(saveProduct.getId())
+        Product findProduct = productRepository.findById(product.getId())
             .orElseThrow(ProductNotFoundException::new);
 
         //then
-        assertThat(findProduct).isEqualTo(saveProduct);
+        assertThat(findProduct).isEqualTo(product);
     }
 
     @Test
     @DisplayName("상품을 데이터베이스에서 수정한다.")
     public void update() {
-        //given
-        ProductRequest productRequest = new ProductRequest("더 나은 코드를 작성하는 간단하고 실질적인 테크닉",
-            "읽기 좋은 코드가 좋은코드다", 1_000L, ProductStatus.SELL.name(),
-            "https://image.aladin.co.kr/product/1610/82/cover500/897914914x_1.jpg", 18_000L);
-        Product product = productRepository.save(productRequest.toProduct());
-        em.clear();
-
         //when
         ProductRequest updateRequest = new ProductRequest(productRequest.getDescription(),
             productRequest.getName(), 0L, ProductStatus.OUT_OF_STOCK.name(),
@@ -74,18 +75,15 @@ class ProductRepositoryTest {
 
         //then
         assertThat(updateProduct.getId()).isEqualTo(product.getId());
+        assertAll(
+            () -> Assertions.assertThat(updateProduct.getStock()).isEqualTo(new Stock(0L)),
+            () -> Assertions.assertThat(updateProduct.getProductStatus()).isEqualTo(ProductStatus.OUT_OF_STOCK)
+        );
     }
 
     @Test
     @DisplayName("상품을 데이터베이스에서 삭제한다.")
     public void delete() {
-        //given
-        ProductRequest productRequest = new ProductRequest("더 나은 코드를 작성하는 간단하고 실질적인 테크닉",
-            "읽기 좋은 코드가 좋은코드다", 1_000L, ProductStatus.SELL.name(),
-            "https://image.aladin.co.kr/product/1610/82/cover500/897914914x_1.jpg", 18_000L);
-        Product product = productRepository.save(productRequest.toProduct());
-        em.clear();
-
         //when
         productRepository.delete(product);
 
