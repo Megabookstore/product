8c2bb0054d0a1b70c1c2fa63342c675f0429b46d
2HyeonHo <dpqlvkvk@naver.com>
2023-01-13 23:13:44 +0900

    test: 인수테스트 공통 코드 추가

--- src/test/java/com/bookstore/common/AcceptanceTest.java
+++ src/test/java/com/bookstore/common/AcceptanceTest.java
@@ -2,8 +2,10 @@ package com.bookstore.common;
 
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.jdbc.Sql;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public abstract class AcceptanceTest {
@@ -11,8 +13,12 @@ public abstract class AcceptanceTest {
     @LocalServerPort
     int port;
 
+    @Autowired
+    private DatabaseCleanup databaseCleanup;
+
     @BeforeEach
     public void setUp() {
         RestAssured.port = port;
+        databaseCleanup.execute();
     }
 }
