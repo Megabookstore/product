ed22521237b2d15a896f3ca0e8386c9b2efce10f
2HyeonHo <dpqlvkvk@naver.com>
2023-01-27 20:58:32 +0900

    feat: 카테고리 도메인 작성

--- src/main/java/com/bookstore/category/domain/Category.java
+++ src/main/java/com/bookstore/category/domain/Category.java
@@ -1,8 +1,38 @@
 package com.bookstore.category.domain;
 
+import com.bookstore.common.domain.Name;
+import javax.persistence.Column;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
 
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
 @Entity
 public class Category {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Category category;
+
+    @Embedded
+    private Name name;
+
+    @Column(nullable = false)
+    @ColumnDefault("false")
+    private boolean isHidden;
 }
